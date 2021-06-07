7819997b550d2121d3bb043838381c1b605cc15d
Eric Chanudet <chanudete@ainfosec.com>
2021-06-07 11:51:46 -0400

    idl: separate files for language specifics
    For example, type conversions will obviously be language specific. Other
    logics may arise as well. Separate the implementation in language
    specific files to keep things in order.
    
    Signed-off-by: Eric Chanudet <chanudete@ainfosec.com>

---
+++ go-idl.go
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: BSD-3-Clause
+package main
+
+var goSingleSigMap = map[string]string{
+	"y": "byte",
+	"b": "bool",
+	"n": "int16",
+	"q": "uint16",
+	"i": "int32",
+	"u": "uint32",
+	"x": "int64",
+	"t": "uint64",
+	"d": "float64",
+	"s": "string",
+	"o": "dbus.ObjectPath",
+	"v": "dbus.Variant",
+	"h": "unixFDIndex",
+}
+
+var goDoubleSigMap = map[string]string{
+	"ay": "[]byte",
+	"ai": "[]int32",
+	"au": "[]uint32",
+	"at": "[]uint64",
+	"as": "[]string",
+	"ao": "[]dbus.ObjectPath",
+}
+
+var goComplexSigMap = map[string]string{
+	"a{ss}":     "map[string]string",
+	"a{sv}":     "map[string]dbus.Variant",
+	"aa{ss}":    "[]map[string]string",
+	"aa{sv}":    "[]map[string]dbus.Variant",
+	"a{sa{sv}}": "map[string]map[string]dbus.Variant",
+}
+
+func GoType(dt string) string {
+	if t, ok := goSingleSigMap[dt]; ok {
+		return t
+	}
+	if t, ok := goDoubleSigMap[dt]; ok {
+		return t
+	}
+	if t, ok := goComplexSigMap[dt]; ok {
+		return t
+	}
+	return "interface{}"
+}
+
+func (a Arg) GoType() string {
+        return GoType(a.Type)
+}
+
+func (e Enum) GoType() string {
+        return GoType(e.Type)
+}
+
+func (p Property) GoType() string {
+        return GoType(p.Type)
+}
